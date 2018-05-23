1f235e7b1eb7fc1b06568c6b9a2f4b86d115af8f
Istaak <fernandoistaak@gmail.com>
2018-05-23 11:00:45 -0500

    working page

--- src/app/main.js
+++ src/app/main.js
@@ -2,145 +2,116 @@ import React, {Component} from 'react';
 import {Hero} from './components/Hero';
 import {Grid, Row, Col} from 'react-bootstrap';
 
+import {GMap} from './components/Map/Map';
+import {CarouselGallery} from './components/CarouselGallery/CarouselGallery';
+import {carouselItems} from '../constants';
+
 export class Main extends Component {
   render() {
     return (
       <div>
         <Grid fluid>
           <Row>
-            <div className="hero-container">
-              <h1 className="text-center title">
-                <small>Estamos contigo</small>
-                en el día más feliz de tu vida
-              </h1>
-
-              <p className="text-center">
-                <button className="btn btn-default">Realizar Pedido</button>
-              </p>
-            </div>
+            <Hero/>
           </Row>
         </Grid>
 
-        <Grid>
+        <Grid fluid>
           <Row>
             <Col xs={12}>
-              <h2>Creamos momentos</h2>
-              <p>
-                Bienvenido a la experiencia Green terraza, diseño Floral para eventos sociales y corporativos.
-                Diseños florales tan especiales como lo son los momentos que tú creas.
-              </p>
+              <div className="home-section">
+                <h2 className="title">Creamos momentos</h2>
+                <p className="description">
+                  Bienvenido a la experiencia Green terraza, diseño Floral para eventos sociales y corporativos.
+                  Diseños florales tan especiales como lo son los <br/>momentos que tú creas.
+                </p>
+              </div>
             </Col>
           </Row>
 
           <Row>
-            <Col xs={4}>
-              <div className="gallery-element">
-                <img className="image img-responsive" src="../assets/img/flor1.jpg"/>
-                <div className="description">
-                  <h3>Sueño de invierno</h3>
-                  <span>Ramo Pomander</span>
-                </div>
-              </div>
-            </Col>
+            <CarouselGallery items={carouselItems}/>
+          </Row>
 
-            <Col xs={4}>
-              <div className="gallery-element">
-                <img className="image img-responsive" src="../assets/img/flor2.jpg"/>
-                <div className="description">
-                  <h3>Sueño de invierno</h3>
-                  <span>Ramo Pomander</span>
-                </div>
-              </div>
-            </Col>
-
-            <Col xs={4}>
-              <div className="gallery-element">
-                <img className="image img-responsive" src="../assets/img/flor3.jpg"/>
-                <div className="description">
-                  <h3>Sueño de invierno</h3>
-                  <span>Ramo Pomander</span>
-                </div>
-              </div>
-            </Col>
-
-            <Col xs={4}>
-              <div className="gallery-element">
-                <img className="image img-responsive" src="../assets/img/flor4.jpg"/>
-                <div className="description">
-                  <h3>Sueño de invierno</h3>
-                  <span>Ramo Pomander</span>
-                </div>
-              </div>
-            </Col>
-
-            <Col xs={4}>
-              <div className="gallery-element">
-                <img className="image img-responsive" src="../assets/img/flor5.jpg"/>
-                <div className="description">
-                  <h3>Sueño de invierno</h3>
-                  <span>Ramo Pomander</span>
-                </div>
-              </div>
-            </Col>
-
-            <Col xs={4}>
-              <div className="gallery-element">
-                <img className="image img-responsive" src="../assets/img/bouquet.jpg"/>
-                <div className="description">
-                  <h3>Sueño de invierno</h3>
-                  <span>Ramo Pomander</span>
-                </div>
+          <Row>
+            <Col>
+              <div className="home-section -image">
+                <h1 className="description">Trato personal</h1>
+                <p>Trabajamos contigo para crearte un estilo propio adaptado a
+                  tus gustos, necesidades y por supuesto acorde a tu presupuesto
+                  respetando tu propio estilo y gusto, tambien contamos con
+                  asesoramiento para llegar a lo que haz soñado para ese día tan
+                  especial.
+                </p>
               </div>
             </Col>
           </Row>
-
           <Row>
             <Col xs={12}>
-              <h2>Eventos</h2>
-              <p>Cuenta con nuestro asesoramiento en la decoraión parcial o total del evento</p>
+              <div className="home-section">
+                <h2 className="title">Eventos</h2>
+                <p className="description">Cuenta con nuestro asesoramiento en la decoraión parcial o total del evento</p>
+              </div>
             </Col>
           </Row>
-
           <Row>
-            <Col xs={3}>
-              <img src="http://via.placeholder.com/150x150"/>
-              <h4>Quince Años</h4>
-              <p>
-                Arreglo de carro
-                Decoración Evento total o parcial
-                Centros de Mesa
-              </p>
+            <Col xs={12} md={3}>
+              <div className="events-item">
+                <img src="../assets/img/icons/ic_zapatos.jpg"/>
+                <h4 className="title">Quince Años</h4>
+                <ul className="list">
+                  <li>Arreglo de carro</li>
+                  <li>Decoración Evento total o parcial</li>
+                  <li>Centros de mesa</li>
+                </ul>
+              </div>
             </Col>
 
-            <Col xs={3}>
-              <img src="http://via.placeholder.com/150x150"/>
-              <h4>Bautizos y Baby Shower</h4>
-              <p>
-                Centros de Mesa Temáticos.
-                Ambientación acorde al gusto del cliente
-              </p>
+            <Col xs={12} md={3}>
+              <div className="events-item">
+                <img src="../assets/img/icons/ic_sonaja.jpg"/>
+                <h4 className="title">Bautizos y Baby Shower</h4>
+                <ul className="list">
+                  <li>Centros de Mesa Temáticos.</li>
+                  <li>Ambientación acorde al gusto del cliente</li>
+                </ul>
+              </div>
             </Col>
 
-            <Col xs={3}>
-              <img src="http://via.placeholder.com/150x150"/>
-              <h4>Bodas</h4>
-              <p>
-                Centros de Mesa
-                Ramo de novia
-                Ramo para la virgen
-                Ramo para aventar
-              </p>
+            <Col xs={12} md={3}>
+              <div className="events-item">
+                <img src="../assets/img/icons/ic_anillos.jpg"/>
+                <h4 className="title">Bodas</h4>
+                <ul className="list">
+                  <li>Centros de Mesa</li>
+                  <li>Ramo de novia</li>
+                  <li>Ramo para la virgen</li>
+                  <li>Ramo para aventar</li>
+                </ul>
+              </div>
             </Col>
-
-            <Col xs={3}>
-              <img src="http://via.placeholder.com/150x150"/>
-              <h4>Eventos Corporativos</h4>
-              <p>
-                Decoración para un evento corporativo
-                Decoración para un evento corporativo
-                Decoración para un evento corporativo
-                Decoración para un evento corporativo
-              </p>
+            <Col xs={12} md={3}>
+              <div className="events-item">
+                <img src="../assets/img/icons/ic_copas.jpg"/>
+                <h4 className="title">Eventos Corporativos</h4>
+                <ul className="list">
+                  <li>Decoración para un evento corporativo</li>
+                  <li>Decoración para la recepción de una oficina</li>
+                  <li>Decoración para Aniversarios</li>
+                  <li>Decoración para Inauguración</li>
+                  <li>Reuniones</li>
+                </ul>
+              </div>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <div className="map">
+                <GMap
+                  lat={59.955413}
+                  lng={30.337844}
+                  />
+              </div>
             </Col>
           </Row>
         </Grid>
